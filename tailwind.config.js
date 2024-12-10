11974c64c350c645979260c6b752890c869d0c5d
T-ESOFT-\T-ESOFT-FORMA-09 <tesoft@.com>
2024-12-10 19:39:58 +0100

    all commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./app/**/*.{js,ts,jsx,tsx,mdx}", 
+    // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // definir ou c'est defini
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
